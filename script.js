21d29290057321d1d58b96caeb23f2f792eb14f5
jotabnu <jonathanvmelo@hotmail.com>
2022-09-04 20:07:24 -0300

    atualização

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function criptografar() {
 
 
     document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto +
-        '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+        '</textarea>' + '<button class="btn-copiar"  onclick="copiar()">Copiar</button>'
 }
 
 function descriptografar() {
@@ -20,7 +20,7 @@ function descriptografar() {
     var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 
     document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto +
-        '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+        '</textarea>' + '<button class="btn-copiar"  onclick="copiar()">Copiar</button>'
 }
 
 function copiar() {
@@ -33,5 +33,5 @@ function copiar() {
 function clean() {
 
     let clearInput = document.querySelector('#input-texto').value = "";
-    textInput.focus();  
+    textInput.focus();
 }
